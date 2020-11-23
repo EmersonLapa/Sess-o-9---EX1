ab829a4a9c3aa8b77045a78282fa13f1d980bbc9
Emerson <emersonlapa@outlook.com>
2020-11-23 07:20:39 -0300

    Upload

---
+++ Entities/Order.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EX2.Entities.Enums;
+using System.Globalization;
+
+namespace EX2.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> Itens { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+
+        }
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+        public void AddItem(OrderItem item)
+        {
+            Itens.Add(item);
+        }
+        public void RemoveItem(OrderItem item)
+        {
+            Itens.Remove(item);
+        }
+
+        public double Total()
+        {
+            double sum = 0.0;
+            foreach (OrderItem item in Itens)
+            {
+                sum += item.SubTotal();
+            }
+            return sum;
+        }
+        
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            Console.WriteLine();
+            stringBuilder.AppendLine("ORDER SUMMARY: ");
+            stringBuilder.AppendLine("Order moment: "  + Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            stringBuilder.AppendLine("Order status: " + Status);
+            stringBuilder.AppendLine("Client: " + Client);
+            stringBuilder.AppendLine("Order items: ");
+            foreach(OrderItem item in Itens)
+            {
+                stringBuilder.AppendLine(item.ToString());
+            }
+            stringBuilder.AppendLine("Total price: $" + Total().ToString("F2", CultureInfo.InvariantCulture));
+            return stringBuilder.ToString();
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using EX2.Entities;
+using EX2.Entities.Enums;
+using System.Globalization;
+namespace EX2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter client data: ");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine(); 
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime date = DateTime.Parse(Console.ReadLine());
+            Client client = new Client(name, email, date);
+            Console.WriteLine("Enter order data: ");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+            Order order = new Order(DateTime.Now, status, client);
+            Console.Write("How many items to this order ? ");
+            int numItems = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= numItems; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data: ");
+                Console.Write("Product name: ");
+                string nameProd = Console.ReadLine();
+                Console.Write("Product price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+                Product product = new Product(nameProd, price);
+                OrderItem orderItem = new OrderItem(quantity, price, product);
+                order.AddItem(orderItem);
+            }
+            Console.WriteLine(order);
+
+        }
+    }
+}
